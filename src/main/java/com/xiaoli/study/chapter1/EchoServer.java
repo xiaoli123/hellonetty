b4a636df252509aa3ffc0788d2439ed1183b2960
liruifeng <liruifeng@mintechai.com>
2020-01-03 14:08:27 +0800

    加注解

--- src/main/java/com/xiaoli/study/chapter1/EchoServer.java
+++ src/main/java/com/xiaoli/study/chapter1/EchoServer.java
@@ -10,6 +10,9 @@ import io.netty.channel.socket.nio.NioServerSocketChannel;
 
 import java.net.InetSocketAddress;
 
+/**
+ * Echo服务端
+ */
 public class EchoServer {
 
     private final int port;
@@ -19,23 +22,30 @@ public class EchoServer {
     }
 
     public void start() throws InterruptedException {
+        //自定义的处理器, 用来接收请求处理请求
         final EchoServerHandler serverHandler = new EchoServerHandler();
+        //指定EventLoopGroup以接收客户端事件,此处用nio
         EventLoopGroup bossGroup = new NioEventLoopGroup();
+        //指定EventLoopGroup以处理客户端事件,此处用nio
         EventLoopGroup workerGroup = new NioEventLoopGroup();
         try {
+            //使用引导程序构建和启动服务
             ServerBootstrap bootstrap = new ServerBootstrap();
-            bootstrap.group(bossGroup, workerGroup)
-                    .channel(NioServerSocketChannel.class)
-                    .localAddress(new InetSocketAddress(port))
-                    .childHandler(new ChannelInitializer<SocketChannel>() {
+            bootstrap.group(bossGroup, workerGroup) //设置loopGroup
+                    .channel(NioServerSocketChannel.class) //设置channel类型
+                    .localAddress(new InetSocketAddress(port)) //设置绑定的端口
+                    .childHandler(new ChannelInitializer<SocketChannel>() { //设置channelInitializer, 并重写它的initChannel方法在pipeline中添加serverHandler
                         @Override
                         protected void initChannel(SocketChannel ch) throws Exception {
                             ch.pipeline().addLast(serverHandler);
                         }
                     });
+            // 异步绑定服务器; 调用sync方法阻塞等待直到绑定完成
             ChannelFuture f = bootstrap.bind().sync();
+            // 获取channel的closeFuture, 并且阻塞当前线程, 知道它完成(不懂)
             f.channel().closeFuture().sync();
         } finally {
+            //释放资源
             bossGroup.shutdownGracefully().sync();
             workerGroup.shutdownGracefully().sync();
         }
--- src/main/java/com/xiaoli/study/chapter1/EchoServerHandler.java
+++ src/main/java/com/xiaoli/study/chapter1/EchoServerHandler.java
@@ -8,9 +8,13 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.util.CharsetUtil;
 
+/**
+ * 自定义处理器
+ */
 @ChannelHandler.Sharable
 public class EchoServerHandler extends ChannelInboundHandlerAdapter {
 
+    // 当读取到信息后, 将读取到的信息回写客户端
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         ByteBuf in = (ByteBuf)msg;
@@ -18,11 +22,13 @@ public class EchoServerHandler extends ChannelInboundHandlerAdapter {
         ctx.write(in);
     }
 
+    // 读取完毕后, 刷新
     @Override
     public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
         ctx.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener(ChannelFutureListener.CLOSE);
     }
 
+    //异常处理
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
         cause.printStackTrace();
